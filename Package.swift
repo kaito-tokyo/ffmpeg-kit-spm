dd3190919f80c9846ec3ec5683f6adb14885b393
Kaito Udagawa <umireon@gmail.com>
2024-02-28 04:32:31 +0900

    Add workflows (#1)
    * Add
    
    * Add
    
    * Update push.yaml
    
    * Update push.yaml
    
    * Update push.yaml
    
    * Update push.yaml
    
    * Create .gitignore
    
    * Add
    
    * Update push.yaml
    
    * Update push.yaml
    
    * Update push.yaml

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+  name: "FFmpegKit",
+  platforms: [.iOS(.v12)],
+  products: [
+    .library(name: "FFmpegKit", targets: [])
+  ],
+  targets: [
+  ]
+)
